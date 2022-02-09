1addae41aa2a95b2eca4a3b900d61aa15cf304fa
Chris Siannas <xrsia@otenet.gr>
2022-02-09 22:14:08 +0200

    Atoi

--- convert-strings.go
+++ convert-strings.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"strconv"
 	"strings"
 )
 
@@ -13,4 +14,13 @@ func main() {
 
 	str = "     52    "
 	fmt.Printf("\n%v Type: %T,Length:%v\n", str, str, len(str))
+	str = strings.Trim(str, " ")
+	fmt.Printf("\n%v Type: %T,Length:%v\n", str, str, len(str))
+
+	num, err := strconv.Atoi(str)
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Printf("%v Type: %T\n", num, num)
+	}
 }
