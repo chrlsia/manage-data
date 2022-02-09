5e3b91a7ce7fd19434b0e8b47b96673f69ee3937
Chris Siannas <xrsia@otenet.gr>
2022-02-09 22:07:54 +0200

    ToUpper,ToLower,Title

--- convert-strings.go
+++ convert-strings.go
@@ -8,4 +8,9 @@ import (
 func main() {
 	str := "Begin by importing the Go standard library strings"
 	fmt.Println(strings.ToUpper(str))
+	fmt.Println(strings.ToLower(str))
+	fmt.Println(strings.Title(str))
+
+	str = "     52    "
+	fmt.Printf("\n%v Type: %T,Length:%v\n", str, str, len(str))
 }
